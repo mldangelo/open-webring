43b2a8e23f05324d00bfa84cb050df88aee2922d
mldangelo <michael.l.dangelo@gmail.com>
2020-12-12 21:37:35 -0500

    add old webring.org index page

---
+++ src/pages/webring.js
@@ -0,0 +1,322 @@
+const WebRing = () => (
+  <div align="center">
+    <center>
+      <table width="570" border="0">
+        <tbody>
+          <tr>
+            <td align="right" valign="middle">
+              <img
+                width="150"
+                height="78"
+                border="0"
+                src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/shape.jpg"
+                alt="Shape of Things to come"
+              />
+            </td>
+            <td width="10"> </td>
+            <td align="left" valign="middle">
+              <img
+                width="385"
+                height="211"
+                border="0"
+                src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/webplus12.jpg"
+                alt="WebRing"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top" width="150">
+              <a href="/web/20000815063035/http://webring.org/index.html#ringworld">
+                <img
+                  alt="RingWorld"
+                  border="0"
+                  height="51"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav1a.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+              <a href="/web/20000815063035/http://webring.org/what.html">
+                <img
+                  alt="What is WebRing?"
+                  border="0"
+                  height="50"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav2.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+              <a href="/web/20000815063035/http://webring.org/join.html">
+                <img
+                  alt="Join WebRing"
+                  border="0"
+                  height="36"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav3.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+              <a href="/web/20000815063035/http://webring.org/manage.html">
+                <img
+                  alt="Ring Management"
+                  border="0"
+                  height="50"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav4.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+              <a href="/web/20000815063035/http://webring.org/help/">
+                <img
+                  alt="Help"
+                  border="0"
+                  height="38"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav5.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+              <a href="/web/20000815063035/http://webring.org/ad_form.html">
+                <img
+                  alt="Advertising"
+                  border="0"
+                  height="36"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav6.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+              <a href="/web/20000815063035/http://webring.org/contact.html">
+                <img
+                  alt="Contact Us"
+                  border="0"
+                  height="48"
+                  src="https://web.archive.org/web/20000815063035im_/http://us.yimg.com/i/wr/nav7.gif"
+                  width="131"
+                />
+              </a>
+              <br />
+            </td>
+            <td align="right" valign="top" width="10">
+              {' '}
+            </td>
+            <td valign="left">
+              <table width="100%" cellPadding="0" cellSpacing="0" border="0">
+                <tbody>
+                  <tr>
+                    <td height="14"> </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <font face="helvetica,arial" size="-1">
+                        <p>
+                          Welcome to WebRing, one of the fastest and most
+                          exciting ways to navigate the World Wide Web! This
+                          completely free service offers easy access to hundreds
+                          of thousands of member websites organized by related
+                          interests into easy-to-travel Rings.
+                        </p>
+                        <p>
+                          You can find Rings that interest you by clicking on
+                          category links in the
+                          <a href="#ringworld">RingWorld Directory</a>, or by
+                          using <a href="#search">RingSearch</a>.
+                        </p>
+                        <p>
+                          WebRing is one of the simplest and most efficient ways
+                          to find content on the Internet. Our member sites are
+                          everywhere. Anytime you find yourself at a WebRing
+                          member page, just click on the navigation buttons or
+                          hypertext links to travel to other sites in the Ring.
+                        </p>
+                      </font>
+                      <p>
+                        <font face="helvetica,arial" size="-1">
+                          Any web site owner can apply to{' '}
+                          <a href="join.html">join an existing Ring</a> or
+                          <a href="/web/20000815063035/http://webring.org/wrnewring1.html">
+                            create a new Ring
+                          </a>
+                          . Rings may be listed in the RingWorld Directory once
+                          they contain at least five sites.
+                        </font>
+                      </p>
+                      <table width="100%" border="0">
+                        <tbody>
+                          <tr>
+                            <td align="center" valign="middle">
+                              <font face="helvetica,arial" size="-1">
+                                <a href="status.html">STATUS REPORT</a>
+                              </font>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="center" valign="middle">
+                              <font face="helvetica,arial" size="-1">
+                                <a href="terms.html">TERMS OF SERVICE</a>
+                              </font>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td align="center" valign="middle">
+                              <font face="helvetica,arial" size="-1">
+                                <a href="privacy.html">
+                                  WEBRING PRIVACY NOTICE
+                                </a>
+                              </font>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <p>
+                        <br />
+                      </p>
+                      <a name="search"> </a>
+                      <a name="ringworld"> </a>
+                      <hr />
+
+                      <div name="Search">
+                        <form
+                          method="post"
+                          action="https://web.archive.org/web/20000815063035/http://search.webring.org/cgi-bin/search?search"
+                        >
+                          <table
+                            width="100%"
+                            cellPadding="2"
+                            cellSpacing="0"
+                            border="0"
+                          >
+                            <tbody>
+                              <tr>
+                                <td>
+                                  <strong>Search for: </strong>
+                                  <font size="-1">
+                                    <input
+                                      type="radio"
+                                      name="mthd"
+                                      value="or"
+                                    />
+                                    Any Word
+                                    <input
+                                      type="radio"
+                                      name="mthd"
+                                      value="and"
+                                      checked=""
+                                    />{' '}
+                                    All Words
+                                    <input
+                                      type="radio"
+                                      name="mthd"
+                                      value="phrase"
+                                    />{' '}
+                                    Exact Phrase
+                                  </font>
+                                  <br />
+                                  <strong>In: </strong>
+                                  <font size="-1">
+                                    <input
+                                      type="checkbox"
+                                      name="ringdesc"
+                                      checked=""
+                                    />{' '}
+                                    Ring Descriptions&nbsp;
+                                    <input
+                                      type="checkbox"
+                                      name="rings"
+                                      checked=""
+                                    />{' '}
+                                    Ring Names &amp; Keywords
+                                  </font>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td align="left" valign="top">
+                                  <font size="3">
+                                    <input name="query" size="26" />
+                                  </font>
+                                  <input type="submit" value="Search" />
+                                  <p />
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </form>
+                      </div>
+
+                      <p>
+                        <br />
+                        <table cellPadding="4" cellSpacing="0" border="0">
+                          <tbody>
+                            <tr>
+                              <td align="left" bgcolor="#1b1b56">
+                                <p align="left">
+                                  <font face="helvetica,arial">
+                                    <b>
+                                      <big>
+                                        RingWorld, the WebRing Directory
+                                      </big>
+                                    </b>
+                                  </font>
+                                </p>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <br />
+                        <font face="helvetica,arial" size="3">
+                          <a href="ringworld/arts.html">
+                            <b>Arts and Humanities</b>
+                          </a>
+                        </font>
+                        <font face="helvetica,arial" size="-1">
+                          <br />
+                          <nobr>
+                            <a href="ringworld/arts/artists.html">Artists</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/children.html">Family</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/countries.html">
+                              Countries and Cultures
+                            </a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/design.html">Design Arts</a>
+                          </nobr>
+                        </font>
+                        <br />
+                        <nobr>
+                          <font face="helvetica,arial" size="-1">
+                            <a href="ringworld/arts/edu.html">Education</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/hum.html">Literature</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/music.html">Music</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/galleries.html">
+                              Museums and Galleries
+                            </a>
+                          </font>
+                        </nobr>
+                        <br />
+                        <nobr>
+                          <font face="helvetica,arial" size="-1">
+                            <a href="ringworld/arts/org.html">Organizations</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/pub.html">Publications</a>
+                            <font color="#ffffff"> ¤ </font>
+                            <a href="ringworld/arts/theater.html">Theater</a>
+                          </font>
+                        </nobr>
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </center>
+  </div>
+);
+
+export default WebRing;
