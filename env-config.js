3a27773e48e47740827829d38048ffd871bf61a7
mldangelo <michael.l.dangelo@gmail.com>
2020-12-12 20:07:43 -0500

    lint

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.GA_TRACKING_ID' : prod ? 'G-3C644WJZEC': '',
+  'process.env.GA_TRACKING_ID': prod ? 'G-3C644WJZEC' : ''
 };
