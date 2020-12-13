191eae6d7a749c8811bea80b33ab7802bfa0a241
mldangelo <michael.l.dangelo@gmail.com>
2020-12-13 04:58:12 -0500

    add footer inspired by original webring inc. footer

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -38,6 +38,46 @@ class MyDocument extends Document {
               { target: 'https://openwebring.com/how-to' }
             ]}
           />
+          <p />
+          <hr width="600" />
+          <div align="center">
+            <center>
+              <table border="0" width="600">
+                <tbody>
+                  <tr>
+                    <td>
+                      <font face="helvetica,arial" size="-2">
+                        Brought to you by{' '}
+                        <a href="https://github.com/mldangelo/open-webring">
+                          Open-Webring.
+                        </a>
+                      </font>
+                    </td>
+                    <td>
+                      <font face="helvetica,arial" size="-2">
+                        Inspired by{' '}
+                        <a href="https://en.wikipedia.org/wiki/Webring#WebRing.com">
+                          Webring Inc. © 2000.
+                        </a>
+                      </font>
+                    </td>
+                    <td>
+                      <p align="center">
+                        <font face="helvetica,arial" size="-2">
+                          © {new Date().getFullYear()} Open-Webring.
+                        </font>
+                      </p>
+                    </td>
+                    <td align="right">
+                      <font face="helvetica,arial" size="-2">
+                        No Rights Reserved
+                      </font>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </center>
+          </div>
         </body>
       </Html>
     );
