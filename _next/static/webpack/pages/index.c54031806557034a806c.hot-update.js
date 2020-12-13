eb8d71c8d296b60f7af6a68f8b1f99489e0b4022
mldangelo <michael.l.dangelo@gmail.com>
2020-12-13 00:05:36 -0500

    Updates

---
+++ _next/static/webpack/pages/index.c54031806557034a806c.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./ring/index.json":
+/*!*************************!*\
+  !*** ./ring/index.json ***!
+  \*************************/
+/*! exports provided: 0, 1, 2, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"[{\\\"slug\\\":\\\"arthena\\\",\\\"name\\\":\\\"Elite Underground Arthena Employees Ring\\\",\\\"description\\\":\\\"This ring contains only high quality personal websites of employees.\\\",\\\"count\\\":4},{\\\"slug\\\":\\\"sample\\\",\\\"name\\\":\\\"Sample Webring\\\",\\\"description\\\":\\\"Short description of the pupose of the web ring.\\\",\\\"count\\\":3},{\\\"slug\\\":\\\"space-potato\\\",\\\"name\\\":\\\"Space Potato Ring\\\",\\\"description\\\":\\\"Step into the depth of potatoes as you have never seen before. This ring contains the personal websites of those who have dedicated themselves to launching potatoes into near-space.\\\",\\\"count\\\":3}]\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3JpbmcvaW5kZXguanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./ring/index.json\n");
+
+/***/ })
+
+})
--- ring/README.md
+++ ring/README.md
@@ -1,3 +1,5 @@
 # Ring
 
 All webrings are defined as JSON here. 
+
+Ring paths are determined by file names. File names should be dash separated strings. 
