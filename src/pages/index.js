e383769b746db4d8e87c64ba1a4fef6281da37ec
mldangelo <michael.l.dangelo@gmail.com>
2020-12-18 18:53:18 -0500

    add more content to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,36 +5,35 @@ import rings from '../../public/rings.json';
 
 const RingWorld = () => (
   <div style={{ width: '570px' }}>
+    <h2>RingWorld Directory</h2>
     <ul>
-      <font face="helvetica">
-        {rings.map((ring) => (
-          <li key={ring.slug}>
-            <b>
-              <Link href={`/ring/${ring.slug}`}>
-                <a>{ring.name}</a>
-              </Link>
-            </b>
-            <font size="-1">
-              {' '}
-              - {ring.count} sites -{' '}
-              <Link href={`/ring/${ring.slug}`}>
-                <a>List</a>
-              </Link>{' '}
-              <Link href={`/ring/${ring.slug}?random}`}>
-                <a>Random</a>
-              </Link>
-            </font>{' '}
-            <Link href={`/ring/${ring.slug}/search`}>
-              <a>
-                <font size="-1">Search Ring</font>
-              </a>
+      {rings.map((ring) => (
+        <li key={ring.slug}>
+          <b>
+            <Link href={`/ring/${ring.slug}`}>
+              <a>{ring.name}</a>
             </Link>
-            <br />
-            <font>{ring.description}</font>
-            <p />
-          </li>
-        ))}
-      </font>
+          </b>
+          <font size="-1">
+            {' '}
+            - {ring.count} sites -{' '}
+            <Link href={`/ring/${ring.slug}`}>
+              <a>List</a>
+            </Link>{' '}
+            <Link href={`/ring/${ring.slug}?random}`}>
+              <a>Random</a>
+            </Link>
+          </font>{' '}
+          <Link href={`/ring/${ring.slug}/search`}>
+            <a>
+              <font size="-1">Search Ring</font>
+            </a>
+          </Link>
+          <br />
+          <font>{ring.description}</font>
+          <p />
+        </li>
+      ))}
     </ul>
   </div>
 );
@@ -57,7 +56,31 @@ const Index = () => (
         </Link>
         .
       </p>
-
+      <div style={{ width: '570px' }}>
+        <p>
+          Welcome to Open-WebRing, an homage to the most exciting way to
+          navigate the World Wide Web!
+        </p>
+        <p>
+          Open-Webring is a free, open-source, MIT lincensed WebRing community
+          designed for hobbyists , enthusiasts, artists, nostalgists, creatives,
+          and for you.
+        </p>
+        <p>
+          You can find Rings that interest you by clicking on category links in
+          the RingWorld Directory, or by using RingSearch.
+        </p>
+        <p>
+          Member sites can be everywhere. Anytime you find yourself at a WebRing
+          member page, just click on the navigation buttons or hypertext links
+          to travel to other sites in the Ring.
+        </p>
+        <p>
+          Any web site owner can apply to join an existing Ring orcreate a new
+          Ring. Rings may be listed in the RingWorld Directory once they contain
+          at least five sites.
+        </p>
+      </div>
       <RingWorld />
     </div>
   </>
