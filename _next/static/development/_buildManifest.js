e59abc0cd31fd68fe69d043379ea70711037da74
mldangelo <michael.l.dangelo@gmail.com>
2020-12-18 18:10:50 -0500

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/how-to":["static\u002Fchunks\u002Fpages\u002Fhow-to.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fhow-to"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
